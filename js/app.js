a1748d3c74bea4f284da0d55c081c1fd5d376efc
Raluca Cretu <ralucacr2000@yahoo.com>
2018-06-03 18:13:31 +0200

    Call Enemy collision method inside Player class and update Player class

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 // Enemies our player must avoid
 var Enemy = function(x,y,velX) {
-    this.x = x*-100;
-    this.y = y*70;
+    this.x = x*-101;
+    this.y = y*78;
     this.velX = velX;
     this.sprite = 'images/enemy-bug.png';
-    this.width = 100;
-    this.height = 170;
+    this.width = 90;
+    this.height = 50;
 };
 
 // Update the enemy's position, required method for game
@@ -39,27 +39,31 @@ Enemy.prototype.collision = function(player) {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-class Player {
-    constructor(x,y){
-        this.x = x*100;
-        this.y = y*80;
+var Player = function (x,y){
+        this.x = x*101;
+        this.y = y*83;
         this.sprite = 'images/char-horn-girl.png';
-        this.width = 100;
-        this.height = 170;
-    };
-    update(){
+        this.width = 90;
+        this.height = 50;
+};
+
+Player.prototype.update = function(){
         if(this.y > 606){
             this.y = 80;
         }
         if (this.x > 505){
             this.x = 100;
         }
-    };
-    render(){
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    };
+        for (let i = 0; i < allEnemies.length; i++) {
+           allEnemies[i].collision(this);
+        }
+};
 
-    handleInput(key){
+Player.prototype.render = function(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key){
         if(key=="left" && this.x > 0){
             this.x -= 100;
         }else if(key=="right" && this.x < 400){
@@ -69,7 +73,6 @@ class Player {
         }else if(key=="down" && this.y < 400){
             this.y += 80;
         }
-    };
 };
 
 
@@ -82,6 +85,8 @@ var Key= function(x, y) {
 Key.prototype.update = function() {
         this.x = Math.floor(Math.random() * 500) + 100;
         this.y = Math.floor(Math.random() * 210) + 70;
+
+        Math.floor(Math.random() * (max - min)) + min;
 };
 
 Key.prototype.render = function() {
@@ -97,9 +102,7 @@ var allEnemies = [new Enemy(0, 1, 1), new Enemy(0, 2, 3), new Enemy(0, 3, 2), ne
 var player = new Player(2, 5);
 var key = new Key(1, 1);
 
-for (let i = 0; i < allEnemies.length; i++) {
-    allEnemies[i].collision(player);
-}
+
 
 
 // This listens for key presses and sends the keys to your
